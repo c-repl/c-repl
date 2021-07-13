35dc09bd8d06640e672a4c3011e60b6d9917454d
Nikhilesh Yadav <nikhileshofficial@gmail.com>
2021-07-13 14:59:17 +0530

    +session

--- server.ts
+++ server.ts
@@ -1,19 +1,31 @@
 // @ts-nocheck
 const express =require('express')
 const repl = require('repl');
+var session = require('express-session')
 var r=require('./commandline').default
 var cors = require('cors')
 async function main(){
-    var cli=new r
     await cli.init()
     const app=express()
+    app.set('trust proxy', 1) // trust first proxy
+    app.use(session({
+      secret: 'keyboard cat',
+      resave: false,
+      saveUninitialized: false,
+      cookie: { secure: true }
+    }))
     app.use(cors())
     app.use(express.json())
-    app.post('/run',async function(req,res,next)
+    app.post('/run',async function(req,res,next){
+        if (!('cli' in express.session))
+            express.session.cli=new r
+        next()
+
+    },async function(req,res,next)
     {
         var code=req.body.code?.trim();
         try{
-            var result=await cli.run(code)
+            var result=await req.session.cli.run(code)
             res.json(result||{class:'done'})
         }
         catch(e)
